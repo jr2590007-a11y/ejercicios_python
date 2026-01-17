70841ae38e771a1b8085b4ff0b1f1a0fa0103cbe
jr2590007-a11y <jr2590007@gmail.com>
2026-01-17 14:39:32 +0000

    agregue el primer codigo

---
+++ 01.py
@@ -0,0 +1,5 @@
+print("leticia tiene 15 años actualmente,¿que edad tendra dentro dentro de 22 años")
+edad_actual=15
+años_futuros=22
+edad_futura=edad_actual + años_futuros 
+print(f"leticia tendra {edad_futura} años dentro {años_futuros} años")
