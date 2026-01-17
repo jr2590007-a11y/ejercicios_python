e489dc0dfd6deaa429e293c0d5b7e343c43b7114
jr2590007-a11y <jr2590007@gmail.com>
2026-01-17 16:06:56 +0000

    elimine un duplicado y edite el 6

--- 02.py
+++
@@ -1,9 +0,0 @@
-print("calcular el total de años que estudio uriel")
-años_preescolar= 2
-años_primaria=6
-años_secundaria=3
-años_bachillerato=3
-años_licenciatura=5
-años_posgrado=3
-total_años_estudio=años_preescolar +años_primaria + años_secundaria +años_bachillerato + años_licenciatura + años_posgrado
-print(f"urielestudio un total de {total_años_estudio} años")
---
+++ 06.py
@@ -0,0 +1,6 @@
+print("Carlos nació en 1978, a la edad 26 años se graduó en la carrera de ingeniería y año después se caso ¿ En qué año se verificaron estos 2 sucesos ?")
+año_nacimiento = 1978
+edad_graduacion = 26
+año_graduacion = año_nacimiento + edad_graduacion
+año_casamiento = año_graduacion
+print("año_casamiento",año_graduacion)
