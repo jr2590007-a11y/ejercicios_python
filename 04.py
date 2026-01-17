cf552ed0b937f498f16bb69e2dc368e2520c0a35
jr2590007-a11y <jr2590007@gmail.com>
2026-01-17 15:40:41 +0000

    agregue ejercicio 4 y 5

---
+++ 04.py
@@ -0,0 +1,9 @@
+print("calcular alño de fallecimiento de efrain")
+casamiento:1988
+nacimiento:1991
+fallecimiento:2005
+año_nacimiento_efrain = 1960
+edad_casamiento = 28
+años_despues_matrimonio_hijo= 3
+edad_hijo_cuando_fallece = 14
+año_casamiento-
---
+++ 05.py
@@ -0,0 +1,6 @@
+print("5. Efraín nació en 1960, se casó a los 28 años, a los 3 años de matrimonio nació su único hijo. Si Efraín falleció cuando su hijo tenía 14 años, ¿en qué año ocurrió su fallecimiento?")
+nacimiento = 1960
+año_casamiento = nacimiento + 28
+nacimiento_hijo = año_casamiento + 3
+año_fallecimiento =nacimiento_hijo + 14
+print("año que murio",año_fallecimiento)
